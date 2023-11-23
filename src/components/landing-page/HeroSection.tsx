758d2dee6ce70cccab616d9b7a513bc8b4b78ec7
Mark Wong <markwong.digital@gmail.com>
2023-11-22 16:48:15 -0800

    Change text

--- src/components/landing-page/HeroSection.tsx
+++ src/components/landing-page/HeroSection.tsx
@@ -8,12 +8,14 @@ const HeroSection = () => {
 			<div className="max-w-3xl py-20 mx-auto text-center flex flex-col items-center">
 				<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
 					Your marketplace for high quality{" "}
-					<span className="text-blue-600">digital assets</span>.
+					<span className="text-blue-600">badminton equipment</span>.
 				</h1>
 
 				<p className="mt-6 text-xl max-w-prose text-muted-foreground">
-					Welcome to Ecommerce Store. Lorem ipsum dolor sit amet consectetur
-					adipisicing elit cum ipsam omnis adipisci ducimus.
+					Elevate Your Game.
+					<br />
+					Lorem ipsum dolor sit amet consectetur adipisicing elit cum ipsam
+					omnis adipisci ducimus.
 				</p>
 
 				<div className="flex flex-col gap-4 mt-6 sm:flex-row">
